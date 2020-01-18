c71090cd98877f2598c08c047266bf806a0fbb58
Ramnath Nair <rnjava@gmail.com>
2020-01-18 10:01:23 -0700

    Spring Template

---
+++ AgilePPMTool/src/main/java/org/openobservability/ppmtool/AgilePpmToolApplication.java
@@ -0,0 +1,13 @@
+package org.openobservability.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilePpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilePpmToolApplication.class, args);
+	}
+
+}
---
+++ AgilePPMTool/src/test/java/org/openobservability/ppmtool/AgilePpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openobservability.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilePpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
